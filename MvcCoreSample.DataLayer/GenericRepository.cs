b9116f7e0d4da7854028554e71f25e6dfda7fc1b
Sirwan Afifi <sir1afifi@gmail.com>
2018-08-03 11:38:02 +0430

    removed extra namespaces

--- MvcCoreSample.DataLayer/GenericRepository.cs
+++ MvcCoreSample.DataLayer/GenericRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 using MvcCoreSample.DomainClasses;
 
 namespace MvcCoreSample.DataLayer
