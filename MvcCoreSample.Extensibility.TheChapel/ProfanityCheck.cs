fd8bee0e04df6d345f1c6c97df6e19615d042f38
Sirwan Afifi <sir1afifi@gmail.com>
2018-08-18 10:30:43 +0430

    added a new module

---
+++ MvcCoreSample.Extensibility.TheChapel/ProfanityCheck.cs
@@ -0,0 +1,42 @@
+﻿using MvcCoreSample.Extensibility.Common;
+
+namespace MvcCoreSample.Extensibility.TheChapel
+{
+    public class ProfanityCheck : ICoreModule
+    {
+        private string[] _badWords;
+
+        public ProfanityCheck()
+        {
+            _badWords = GetProfanityWords();
+        }
+        
+        public void Initialize(MvcCoreSampleModuleEvents moduleEvents)
+        {
+            moduleEvents.PreSubmitMovie += OnPreSubmitMovie;
+        }
+
+        private void OnPreSubmitMovie(PreSubmitMovieEventArgs e)
+        {
+            e.Title = RemoveProfanity(e.Title);
+        }
+
+        private string RemoveProfanity(string text)
+        {
+            string newText = text;
+
+            foreach (var badWord in _badWords)
+                newText = newText.Replace(badWord, "$%!@&*#$");
+
+            return newText;
+        }
+
+        private string[] GetProfanityWords()
+        {
+            return new string[]
+            {
+                "filth", "flarn"
+            };
+        }
+    }
+}
